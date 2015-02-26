d8743be870b6a7be675e06c304b8cb6c4a54e2a6
worace <horace.d.williams@gmail.com>
2015-02-26 11:40:12 -0700

    add very slow solution for 5

--- src/euler/005.clj
+++ src/euler/005.clj
@@ -6,32 +6,6 @@
                       (= 0 (mod x div))) mods))
    false))
 
-
-
-(defn possible-factors [n]
- (remove (fn [x] (= x n)) (range 2 (+ 1 (math/sqrt n))))
-  
- 
-;; range 2 (+ 1 (math/sqrt n))
-)
-
-(defn is-prime [n]
-  (not (divisible n (possible-factors n)))
-)
-
-(defn divisors-up-to [n]
-  (filter is-prime (range 1 (+ n 1))))
-
-(defn lowest-multiple [n]
-  (loop [multiple 1 divisors (divisors-up-to n)]
-    (if (empty? divisors)
-      multiple
-      (let [[next & divisors] divisors]
-        (if (divisible multiple [next])
-          (recur multiple divisors)
-          (recur (* next multiple) divisors))))))
-
-
 (defn divis-by-all [x divisors]
   (every? (fn [divisor] (divisible x [divisor])) divisors))
 

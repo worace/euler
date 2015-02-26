49efcc2e0da0138616a6477cc9f8bcbcb74c95b5
worace <horace.d.williams@gmail.com>
2015-02-25 20:13:29 -0700

    add feeble attempt at an euler solution in clojure

---
+++ src/euler/001.clj
@@ -0,0 +1,20 @@
+(ns euler.001)
+
+(defn pizza
+  []
+  (println "hi"))
+
+(defn divisible [x & mods]
+  (or (some true? (map (fn [div]
+                      (= 0 (mod x div))) mods))
+   false))
+
+
+
+(defn multiples-up-to
+  [n]
+  (filter (fn [x]
+            (divisible x 3 5))
+          (range 1 n)))
+
+

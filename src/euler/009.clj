6195f85c7bf677f76aee0e9cede4db82ca4d683a
worace <horace.d.williams@gmail.com>
2015-02-28 18:47:35 -0700

    finding target sum triples using ternary trees

--- src/euler/009.clj
+++ src/euler/009.clj
@@ -1,11 +1,6 @@
 (ns euler.009
  (:require [clojure.math.combinatorics :as combo]))
 
-;;                  [[1, 2, 2], [2, 1, 2], [2, 2, 3]],
-;;                  [[1, -2, 2], [2, -1, 2], [2, -2, 3]],
-;;                  [[-1, 2, 2], [-2, 1, 2], [-2, 2, 3]]
-
-
 (def init-matrices
   [[[1 2 2] [2 1 2] [2 2 3]]
    [[1 -2 2] [2 -1 2] [2 -2 3]]
@@ -17,26 +12,8 @@
   (let [[a b c] v]
        (= (reduce + (map square [a b])) (square c))))
 
-;; (2m,m^2-1,m^2+1), 	
-(defn triple [m n]
-  [(- (* n n) (* m m)) (* 2 m n) (+ (* n n) (* m m))])
-
 (defn greater-than [left right]
   (= 1 (compare left right)))
-
-
-(defn triples-up-to [n]
-  (loop [iteration 1 triples []]
-    (if (greater-than (reduce + (last triples)) n)
-      triples
-      (recur (+ 1 iteration) (conj triples (triple iteration (+ 1 iteration)))))))
-
-(defn crosses-in-range [n]
-  (combo/cartesian-product (range 1 n) (range 1 n)))
-
-(defn triple-summing-to [n]
-  (first (filter (fn [x] (= x n)) (triples-up-to n))))
-
 
 (defn vector-product [v1 v2]
   (map (fn [pair] (reduce * pair)) (map vector v1 v2)))
@@ -46,3 +23,32 @@
 
 (defn matrix-mult [matrix vector]
   (map (fn [v] (reduce + v)) (cross-mult matrix vector)))
+
+(defn child-triples
+  ([triple num-generations]
+   (loop [seeds [triple] children [] count (+ 1 num-generations)]
+     (if (= count 0)
+       children
+       (recur (mapcat child-triples seeds) seeds (- count 1)))))
+  ([triple]
+   (map (fn [m] (matrix-mult m triple)) init-matrices)))
+
+(defn sum-triples [triples]
+  (map (fn [t] (reduce + t)) triples))
+
+(defn triple-factor-of-target [target triples]
+  (first (filter (fn [triple] (= 0 (mod target (reduce + triple)))) triples)))
+
+(defn triple-scaled-to [n triple]
+  (let [[factor] [(/ n (reduce + triple))]]
+    (vector-product triple [factor factor factor])))
+
+(defn triple-summing-to [n]
+  (loop [target n iter 0 triples (child-triples [3 4 5] 0)]
+    (let [[match] [(triple-factor-of-target target triples)]]
+      (if match
+        (triple-scaled-to target match)
+        (if (every? (fn [t] (greater-than (reduce + t) target)) triples)
+          nil
+          (recur target (+ 1 iter) (child-triples [3 4 5] (+ 1 iter))))))))
+
--- test/euler/prob_nine_test.clj
+++ test/euler/prob_nine_test.clj
@@ -25,4 +25,28 @@
 
 (deftest test-produces-triples-from-base-matrices
   (testing "multiplying base matrices by initial 3-4-5 triple produces more triples"
-    (is (triple? (matrix-mult (first init-matrices) [3 4 5])))))
+    (is (every? triple? (map (fn [m] (matrix-mult m [3 4 5])) init-matrices)))))
+
+(deftest test-produces-new-triples-from-a-seed
+  (testing "given seed triple it makes 3 new one"
+    (is (= 3 (count (child-triples [3 4 5]))))
+    (is (every? triple? (child-triples [3 4 5])))))
+
+(deftest test-produces-n-iterations-when-asked
+  (testing "given a triple and n it finds that # of generations"
+    (is (= 1 (count (child-triples [3 4 5] 0))))
+    (is (= 3 (count (child-triples [3 4 5] 1))))
+    (is (= 9 (count (child-triples [3 4 5] 2))))
+    (is (= 27 (count (child-triples [3 4 5] 3))))
+    (is (every? triple? (child-triples [3 4 5] 5)))))
+
+(deftest test-triple-summing-to
+  (testing "given a target sum, finds a triple summing to that number"
+    (is (= [3 4 5] (triple-summing-to 12)))
+    (is (= [7 24 25] (triple-summing-to 56)))
+    (is (= [375 200 425] (triple-summing-to 1000)))))
+
+(deftest test-factored-triple
+  (testing "it multiplies a triple by appropriate factor to match target"
+    (is (= [6 8 10] (triple-scaled-to 24 [3 4 5])))))
+

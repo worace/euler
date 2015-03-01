aacb1d841e2bf55bfb7010acc4fb64eb6679e127
worace <horace.d.williams@gmail.com>
2015-02-28 22:14:23 -0700

    got a slow p10 going

--- 010.clj
+++ 010.clj
@@ -1,4 +1,5 @@
 (ns euler.010 (:require [clojure.math.numeric-tower :as math]))
+;; find sum of all primes below 2 million
 
 (defn divisible [x mods]
   (or (some true? (map (fn [div]
@@ -14,12 +15,28 @@
 )
 
 (defn non-multiples [coll n]
-  (filter (fn [x] (not (divisible x n))) coll))
+  (filter (fn [x] (not (divisible x [n]))) coll))
 
 (defn primes-up-to [n]
   (loop [primes [] [next & remaining] (range 1 (+ 1 n))]
     (if (empty? remaining)
       primes
       (if (is-prime next)
-        (recur (conj primes next) [[next & remaining] (non-multiples remaining next)])
-        (recur primes [[next & remaining] remaining])))))
+        (recur (conj primes next) (non-multiples remaining next))
+        (recur primes remaining)))))
+
+(defn up-primes [n]
+  (loop [primes [2] current 3]
+    (if (= n current)
+      primes
+       (if (divisible current primes)
+         (recur primes (+ 1 current))
+         (recur (conj primes current) (+ 1 current))))))
+
+
+(defn alt-primes [n]
+  (filter is-prime (range 1 (+ 1 n))))
+
+(defn sum-primes-to [n]
+  (reduce + (alt-primes n)))
+;;142913828922 in 35 seconds using naive solution
